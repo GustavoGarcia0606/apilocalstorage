6a8f0a2d0782dd1b4f4f1b2190e09f081a99713f
GustavoGarcia <gu.garciamarinho@gmail.com>
2025-05-27 19:57:09 -0300

    update

---
+++ js/scriplh.js
@@ -0,0 +1,9 @@
+let tarefas = JSON.parse(localStorahge.getItem("tarefas")) || [];
+
+const inputTarefa = document.querySelector(".campo-tarefa");
+const botaoAdicionar = document.querySelector(".botao-adicionar");
+const listaTarefas = document.querySelector(".lista-tarefas");
+
+
+
+
